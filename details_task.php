244fc35b6afd492fd9371d55008d1b84eb52dbc7
bamagid <bamagid60@gmail.com>
2023-10-21 06:16:09 +0000

    ajout dynamique du contenu par le php et pour afficher uniquement la tache choisie avec le boutoun voir plus dans le code html et aussi l'ajout des formulaires de changement de statut ou de supresson

--- details_task.php
+++ details_task.php
@@ -1,30 +1,55 @@
+<?php
+session_start();
+include('config.php');
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    if (isset($_POST['details'])) {
+        $id=intval($_POST['details']);
+        $_SESSION["id"]=$id;
+        $detail=$bdd->prepare("SELECT * FROM taches WHERE id=:id");
+        $detail->execute(array('id'=>$id));
+        $details= $detail->fetchAll();
+
+    }else{header("location:dashbord.php");}
+}  
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Details de tâches</title>
     <link rel="stylesheet" href="details_task.css">
 </head>
+
 <body>
-        <div class="header">
-            <h1>Gestion de mes Tâches</h1><br>
-            <p>Saidou Diallo</p>
+    <div class="header">
+        <h1>Gestion de mes Tâches</h1><br>
+        <p><?= $_SESSION['utilisateur']['nom'];?></p>
+    </div>
+    <div class="taches">
+        <?php
+        foreach ($details as $detail) { 
+            echo'
+         <h2>'.$detail['titre'].' </h2><br>
+        <div class="color">
+            <p class="p1"><b>Priorité : '.$detail['priorite'].'   </b></p>
+            <p class="p2"><b>Statut : '.$detail['statut'].' </b></p>
+            <p class="echeance"> Echeance :  '.$detail['echeance'].'</p>
         </div>
-        <div class="taches">
-                <h2>Preparation d'un rapport de vente </h2><br>
-                <div class="color">
-                <p class="p1"><b>Priorité :Haute   </b></p>
-                <p class="p2"><b>Statut :En cours  </b></p>
-                </div>
-                <p>Recueillir les données de vente , generer des graphiques et rediger un rapport detaillée. </p>
-                <div class="manage_task">
-                    <form action="#">
-                    <button type="submit" class="button1"> Marquer comme terminée</button>
-                    <button type="submit" class="button2"> Supprimer la tâche</button>
-                    </form>
-                </div>
+        <p> '.$detail['description'].'</p>
+        
+        <div class="manage_task">
+            <form action="manage_task.php" method="POST">
+                <button type="submit" class="button1" name="done"> Marquer comme terminée</button>
+                <button type="submit" class="button2" name="delete"> Supprimer la tâche</button>
+            </form>
         </div>
-        <a href="#">Retour a la liste des tâches</a>
+    ';
+    }?>
+    </div>
+    <a href="dashbord.php">Retour a la liste des tâches</a>
 </body>
+
 </html>
