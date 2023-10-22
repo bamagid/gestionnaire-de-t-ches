675cf062b208f26de585588553e0615169a24735
bamagid <bamagid60@gmail.com>
2023-10-22 04:28:31 +0000

    mise a ajour des documents

--- details_task.php
+++ details_task.php
@@ -1,30 +1,44 @@
+<?php
+require_once("actions/manage_task.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Details de tâches</title>
-    <link rel="stylesheet" href="details_task.css">
+    <link rel="stylesheet" href="styles/details_task.css">
 </head>
+
 <body>
-        <div class="header">
-            <h1>Gestion de mes Tâches</h1><br>
-            <p>Saidou Diallo</p>
+    <div class="header">
+        <h1>Gestion de mes Tâches</h1><br>
+        <p><?= $_SESSION['utilisateur']['nom'];?></p>
+    </div>
+    <div class="taches">
+        <?php
+        foreach ($details as $detail) { 
+            echo'
+         <h2>'.$detail['titre'].' </h2><br>
+        <div class="color">
+            <p class="p1"><b>Priorité : '.$detail['priorite'].'   </b></p>
+            <p class="p2"><b>Statut : '.$detail['statut'].' </b></p>
+            <p class="echeance"> Echeance :  '.$detail['echeance'].'</p>
         </div>
-        <div class="taches">
-                <h2>Preparation d'un rapport de vente </h2><br>
-                <div class="color">
-                <p class="p1"><b>Priorité :Haute   </b></p>
-                <p class="p2"><b>Statut :En cours  </b></p>
-                </div>
-                <p>Recueillir les données de vente , generer des graphiques et rediger un rapport detaillée. </p>
-                <div class="manage_task">
-                    <form action="#">
-                    <button type="submit" class="button1"> Marquer comme terminée</button>
-                    <button type="submit" class="button2"> Supprimer la tâche</button>
-                    </form>
-                </div>
+        <p> '.$detail['description'].'</p>
+        
+        <div class="manage_task">
+            <form action="manage_task.php" method="POST">
+                <button type="submit" class="button1" name="done"> Marquer comme terminée</button>
+                <button type="submit" class="button2" name="delete"> Supprimer la tâche</button>
+            </form>
         </div>
-        <a href="#">Retour a la liste des tâches</a>
+    ';
+    }?>
+    </div>
+    <a href="dashbord.php">Retour a la liste des tâches</a>
 </body>
+
 </html>
--- pswd_reset.php
+++ pswd_reset.php
@@ -1,21 +1,25 @@
+<?php 
+require_once("actions/reset_action.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset password</title>
-    <link rel="stylesheet" href="pswd_reset.css">
+    <link rel="stylesheet" href="styles/pswd_reset.css">
 </head>
 <body>
-    <form action="#">
-            <h2 class="h21">Reinitialiser mon mot de passe</h2><br>
-            <label for="email1">Email:</label><br>
-            <input type="email" name="email1" class="entrer" autocomplete="off" pattern="[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}" required><br>
-            <label for="password1">Nouveau mot de passe:</label><br>
-            <input type="password" name="password1" class="entrer" required><br>
-            <label for="password2">Confirmation:</label><br>
-            <input type="password" name="password2" class="entrer" required><br>
-            <button type="submit" name="reset1">Reinitialiser</button>
-    </form>
+    <form action="pswd_reset.php" method="post">
+        <h2 class="h21">Reinitialiser mon mot de passe</h2><br>
+        <label for="email1">Email:</label><br>
+        <input type="email" name="email1" class="entrer" autocomplete="off" pattern="[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}" required><br>
+        <label for="passwordr1">Nouveau mot de passe:</label><br>
+        <input type="password" name="passwordr1" class="entrer" required><br>
+        <label for="passwordr2">Confirmation:</label><br>
+        <input type="password" name="passwordr2" class="entrer" required><br>
+        <button type="submit" name="reset1">Reinitialiser</button>
+    </form><a href="index.php">Retour sur la page d'accueil</a>
 </body>
 </html>
