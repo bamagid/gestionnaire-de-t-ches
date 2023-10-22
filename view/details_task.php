86f0fb534176d2b2e120d4e6d7b81f6d07443cf5
bamagid <bamagid60@gmail.com>
2023-10-22 04:12:39 +0000

    reorganisation de la structures du code par dossier ce dossier nommé view contion tous les code html qui vont s'afficher a l'utilisateur

---
+++ view/details_task.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+include('config.php');
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    if (isset($_POST['details'])) {
+        $id=intval($_POST['details']);
+        $_SESSION["id"]=$id;
+        $detail=$bdd->prepare("SELECT * FROM taches WHERE id=:id");
+        $detail->execute(array('id'=>$id));
+        $details= $detail->fetchAll();
+
+    }else{header("location:dashbord.php");}
+}  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Details de tâches</title>
+    <link rel="stylesheet" href="details_task.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1>Gestion de mes Tâches</h1><br>
+        <p><?= $_SESSION['utilisateur']['nom'];?></p>
+    </div>
+    <div class="taches">
+        <?php
+        foreach ($details as $detail) { 
+            echo'
+         <h2>'.$detail['titre'].' </h2><br>
+        <div class="color">
+            <p class="p1"><b>Priorité : '.$detail['priorite'].'   </b></p>
+            <p class="p2"><b>Statut : '.$detail['statut'].' </b></p>
+            <p class="echeance"> Echeance :  '.$detail['echeance'].'</p>
+        </div>
+        <p> '.$detail['description'].'</p>
+        
+        <div class="manage_task">
+            <form action="manage_task.php" method="POST">
+                <button type="submit" class="button1" name="done"> Marquer comme terminée</button>
+                <button type="submit" class="button2" name="delete"> Supprimer la tâche</button>
+            </form>
+        </div>
+    ';
+    }?>
+    </div>
+    <a href="dashbord.php">Retour a la liste des tâches</a>
+</body>
+
+</html>
